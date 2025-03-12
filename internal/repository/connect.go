068376a836bcaff8a30086dcf053d6aecb913f45
Павел Сашков <paul.sashkov@gmail.com>
2025-03-12 17:31:44 +0300

    init tasks TABLE

--- internal/repository/connect.go
+++ internal/repository/connect.go
@@ -3,6 +3,8 @@ package repository
 import (
 	"context"
 	"log"
+	"os"
+	"path"
 
 	"github.com/jackc/pgx/v5"
 )
@@ -10,12 +12,31 @@ import (
 func ConnectDB() *pgx.Conn {
 	conn, err := pgx.Connect(context.Background(), "postgres://tasks:tasks@localhost:5432/dbname?sslmode=disable")
 	if err != nil {
-		log.Fatalf("Ошибка подключения к БД: %v", err)
+		log.Fatalf("ошибка подключения к БД: %v", err)
 	}
+	// проверка соединения
 	err = conn.Ping(context.Background())
 	if err != nil {
-		log.Fatalf("Невозможно установить соединение с БД: %v", err)
+		log.Fatalf("невозможно установить соединение с БД: %v", err)
 	}
 	log.Println("Подключение с базой данных установлено")
+
+	filePath := "migration/001_init.sql"
+	workDir, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("ошибка получения рабочего каталога: %v", err)
+	}
+	sqlFile := path.Join(workDir, filePath)
+	sqlBytesFile, err := os.ReadFile(sqlFile)
+	if err != nil {
+		log.Fatalf("ошибка чтения файла %s: %v", sqlFile, err)
+	}
+
+	sqlReadFile := string(sqlBytesFile)
+	_, err = conn.Exec(context.Background(), sqlReadFile)
+	if err != nil {
+		log.Fatalf("ошибка выполнения SQL запроса: %v", err)
+	}
+
 	return conn
 }
