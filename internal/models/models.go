def6d03032211926d09818c90709bde0e43d0807
Павел Сашков <paul.sashkov@gmail.com>
2025-03-12 18:56:39 +0300

    add task struct

---
+++ internal/models/models.go
@@ -0,0 +1,10 @@
+package models
+
+type Task struct {
+	Id          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Status      string `json:"status"`
+	CreatedAt   string `json:"created_at"`
+	UpdatedAt   string `json:"updated_at"`
+}
